96f8d4b3438ebd39d116609df08878c96afedbdb
Guy Dunton <guydunton@economist.com>
2020-03-13 14:44:19 +0000

    Add comments to canvas_writer

--- src/canvas/canvas_writer.rs
+++ src/canvas/canvas_writer.rs
@@ -4,23 +4,24 @@ use std::fs::File;
 use std::io::BufWriter;
 
 pub fn save_canvas(canvas: Canvas, path: String) -> Result<(), png::EncodingError> {
+    // Create file & writer
     let out_path = Path::new(&path);
     let file = File::create(out_path)?;
     let buf_writer = BufWriter::new(file);
-
+    
+    // Create png writer
     let mut encoder = png::Encoder::new(
         buf_writer, 
         canvas.width() as u32, 
         canvas.height() as u32
     );
-
     encoder.set_color(png::ColorType::RGBA);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header()?;
 
-    let data = canvas.get_save_buffer();
-
-    writer.write_image_data(&data)?;
+    // Write image data
+    let image_data = canvas.get_save_buffer();
+    writer.write_image_data(&image_data)?;
 
     Ok(())
 }
